70255edc486e5c53fc1e6beac10aadbab8627704
Den-R3R5 <deniskostin235@gmail.com>
2026-05-20 00:26:54 +1100

    update

--- gendiff/gendiff.py
+++ gendiff/gendiff.py
@@ -1,2 +1,26 @@
-def gendiff(in1, in2):
-    return print(f"1st input is {in1}, 2nd - {in2}")
+import json
+
+
+def _load_and_sort_files(first_file, second_file):
+    first_file = json.load(open(first_file))
+    second_file = json.load(open(second_file))
+    return dict(sorted(first_file.items())), dict(sorted(second_file.items()))
+
+
+def generate_diff(first_file, second_file):
+    first_file, second_file = _load_and_sort_files(first_file, second_file)
+    keys_massive = sorted(first_file.keys() | second_file.keys())
+    result = ["{"]
+    for key in keys_massive:
+        if key in first_file and key in second_file:
+            if first_file[key] == second_file[key]:
+                result.append(f"    {key}: {first_file[key]}")
+            else:
+                result.append(f"  - {key}: {first_file[key]}")
+                result.append(f"  + {key}: {second_file[key]}")
+        elif key in first_file:
+            result.append(f"  - {key}: {first_file[key]}")
+        else:
+            result.append(f"  + {key}: {second_file[key]}")
+    result.append("}")
+    return "\n".join(result)
