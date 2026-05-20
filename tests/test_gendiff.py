4126c506bdadb8bc408d3267a52769568c37baf8
Den-R3R5 <deniskostin235@gmail.com>
2026-05-21 00:34:51 +1100

    yml support

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from gendiff.gendiff import generate_diff
+from gendiff.parse import parse
 
 
 def get_test_data_pathlib(filename):
@@ -11,7 +12,15 @@ def read_file(filename):
     return get_test_data_pathlib(filename).read_text()
 
 
-def test_gendiff():
+def test_gendiff_json():
     file1 = get_test_data_pathlib("file1.json")
     file2 = get_test_data_pathlib("file2.json")
+    file1, file2 = parse(file1, file2)
+    assert generate_diff(file1, file2) == read_file("result.txt")
+
+
+def test_gendiff_yml():
+    file1 = get_test_data_pathlib("file1.yml")
+    file2 = get_test_data_pathlib("file2.yml")
+    file1, file2 = parse(file1, file2)
     assert generate_diff(file1, file2) == read_file("result.txt")
