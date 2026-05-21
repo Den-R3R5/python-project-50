adc2d693d601ab730abb05d48526ebdd535c36af
Den-R3R5 <deniskostin235@gmail.com>
2026-05-21 23:21:37 +1100

    refactor: line breaks

--- gendiff/formaters/plain_formater.py
+++ gendiff/formaters/plain_formater.py
@@ -20,12 +20,15 @@ def plain(tree, depth=1, directory=""):
             result.append(f"{inner}")
         elif node_type == "changed":
             result.append(
-                f"Property '{directory}{key}' was updated. From {_format(node['value_old'], depth)} to {_format(node['value_new'], depth)}"
+                f"Property '{directory}{key}' was updated. "
+                f"From {_format(node['value_old'], depth)} "
+                f"to {_format(node['value_new'], depth)}"
             )
         elif node_type == "deleted":
             result.append(f"Property '{directory}{key}' was removed")
         elif node_type == "added":
             result.append(
-                f"Property '{directory}{key}' was added with value: {_format(node['value'], depth)}"
+                f"Property '{directory}{key}' was added "
+                f"with value: {_format(node['value'], depth)}"
             )
     return "\n".join(result)
