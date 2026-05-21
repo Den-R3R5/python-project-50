d166ee2b4422b67d8ea1f532243f159bc8682e5a
Den-R3R5 <deniskostin235@gmail.com>
2026-05-21 23:30:46 +1100

    fix: remove debug message

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -4,7 +4,6 @@ from gendiff import arg_parse, generate_diff
 def main():
     args = arg_parse()
     diff = generate_diff(args.first_file, args.second_file, args.format)
-    print(args.format)
     print(diff)
 
 
