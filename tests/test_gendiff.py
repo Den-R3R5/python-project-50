c660f84fce0989d845018d2e97f79d07c862d3f1
Den-R3R5 <deniskostin235@gmail.com>
2026-05-21 20:58:38 +1100

    update

--- tests/test_gendiff.py
+++ tests/test_gendiff.py
@@ -12,15 +12,29 @@ def read_file(filename):
     return get_test_data_pathlib(filename).read_text()
 
 
-def test_gendiff_json():
+def test1_gendiff_json():
     file1 = get_test_data_pathlib("file1.json")
     file2 = get_test_data_pathlib("file2.json")
     file1, file2 = parse(file1, file2)
-    assert generate_diff(file1, file2) == read_file("result.txt")
+    assert generate_diff(file1, file2) == read_file("result1-2.txt")
 
 
-def test_gendiff_yml():
+def test1_gendiff_yml():
     file1 = get_test_data_pathlib("file1.yml")
     file2 = get_test_data_pathlib("file2.yml")
     file1, file2 = parse(file1, file2)
-    assert generate_diff(file1, file2) == read_file("result.txt")
+    assert generate_diff(file1, file2) == read_file("result1-2.txt")
+
+
+def test2_gendiff_json():
+    file1 = get_test_data_pathlib("file3.json")
+    file2 = get_test_data_pathlib("file4.json")
+    file1, file2 = parse(file1, file2)
+    assert generate_diff(file1, file2) == read_file("result3-4.txt")
+
+
+def test2_gendiff_yml():
+    file1 = get_test_data_pathlib("file3.yml")
+    file2 = get_test_data_pathlib("file4.yml")
+    file1, file2 = parse(file1, file2)
+    assert generate_diff(file1, file2) == read_file("result3-4.txt")
