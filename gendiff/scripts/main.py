2b68723034abd4c967c9e912fe50d40d25fc3744
Den-R3R5 <deniskostin235@gmail.com>
2026-05-21 22:40:06 +1100

    format integration

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -4,7 +4,8 @@ from gendiff import arg_parse, generate_diff, parse
 def main():
     args = arg_parse()
     first_file, second_file = parse(args.first_file, args.second_file)
-    diff = generate_diff(first_file, second_file)
+    diff = generate_diff(first_file, second_file, args.format)
+    print(args.format)
     print(diff)
 
 
