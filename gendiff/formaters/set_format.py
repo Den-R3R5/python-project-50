b4e11f7a4e6c5c57f7b40d816827f6f917b56dc0
Den-R3R5 <deniskostin235@gmail.com>
2026-05-22 05:46:18 +1100

    feat: add json format

--- gendiff/formaters/set_format.py
+++ gendiff/formaters/set_format.py
@@ -1,3 +1,4 @@
+from .json_formater import to_json
 from .plain_formater import plain
 from .stylish_formater import stylish
 
@@ -5,4 +6,6 @@ from .stylish_formater import stylish
 def set_format(tree, format_name):
     if format_name == "plain":
         return plain(tree)
+    if format_name == "json":
+        return to_json(tree)
     return stylish(tree)
